0c570d9ece5ce0d51236cd56068b15f7c133e347
bartosian <bartosian1989@gmail.com>
2020-04-26 14:07:33 -0400

    Added bubble sort in js

--- checkDuplicates.js
+++ checkDuplicates.js
@@ -11,3 +11,25 @@ function hasDuplictaes(arr) {
 
     return;
 }
+
+function bubbleSort(arr) {
+    let unchecked_id = arr.length - 1,
+        sorted = false;
+
+    while(!sorted) {
+        sorted = true;
+
+        for(let i = 0; i <= unchecked_id; i++) {
+            if(arr[i] > arr[i + 1]) {
+                sorted = false;
+                let tmp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+
+        unchecked_id -= 1;
+    }    
+    
+    return arr;
+}
