09d8399896845f81fdbf30e767900dd5faae7814
bartosian <bartosian1989@gmail.com>
2020-05-02 14:39:35 -0400

    Added graph algs in js

--- graph.js
+++ graph.js
@@ -1,38 +1,67 @@
-class Person {
+class City {
     constructor(name) {
         this.name = name;
-        this.friends = [];
-        this.visited = false;
+        this.routes = {};
     }
 
-    addFriend(name) {
-        this.friends.push(name);
-    }
-
-    traverseAllFriends() {
-        let resetFriends = [this],
-            queue = [this];
-            
-        this.visited = true;    
-        
-        while(queue.length) {
-            let currentPerson = queue.unshift(),
-                { friends } = currentPerson;
-            
-            console.log(currentPerson.name);           
-
-            for(let i = 0; i < friends.length; i++) {
-                let currentFriend = friends[i];
-
-                if(!currentFriend.visited) {
-                    queue.push(currentFriend);
-                    resetFriends.push(currentFriend);
-                    friends.visited = true
-                }
-            }
-        }
-        
-        resetFriends = resetFriends.map(val => val.visited = false);
+    addRoute(city, price) {
+        this.routes[city] = price;
     }
 }
 
+let atlanta = new City("Atlanta"),
+    boston = new City("Boston"),
+    chicago = new City("Chicago"),
+    denver = new City("Denver"),
+    el_paso = new City("El Paso");
+
+atlanta.add_route("boston", 100);
+atlanta.add_route("denver", 160);
+boston.add_route("denver", 180);
+boston.add_route("chicago", 120);
+chicago.add_route("el_paso", 80);
+denver.add_route("chicago", 40);
+denver.add_route("el_paso", 140);
+
+function dijkstra(startCity, otherCities) {
+    let routesTo_cities = {};
+
+    routesTo_cities[startCity.name] = [0, startCity.name];
+
+
+    for(let i = 0; i < otherCities.length; i++) {
+
+        routesTo_cities[otherCities[i].name] = [Infinity, null];
+    }
+
+    visitedCities = [];
+
+    currentCity = startCity;
+
+    while(currentCity) {
+        visitedCities.push(currentCity);
+
+       for (let [name, price] of Object.items(currentCity)) {
+           if(routesTo_cities[name][0] < (price + routesTo_cities[currentCity])) {
+               routesTo_cities[name] = [price + routesTo_cities[currentCity], currentCity];
+           }
+       }
+
+    }
+
+    currentCity = null;
+
+    cheapestRoutefromCurrentCity = null;
+
+
+    for (let [name, price] of Object.items(routesTo_cities)) {
+        if(price < cheapestRoutefromCurrentCity && !visitedCities.includes(name)) {
+            currentCity = name;
+            cheapestRoutefromCurrentCity = price;
+        }
+    }
+
+    return routesTo_cities;
+
+
+}
--- graph.rb
+++ graph.rb
@@ -38,3 +38,69 @@ class Person
   end
 
 end
+
+class City
+
+  attr_accessor :name, :routes
+
+  def initialize(name)
+    @name = name
+    @routes = {}
+  end
+
+  def add_route(city, price)
+    @routes[city] = price
+  end
+
+end
+
+atlanta = City.new("Atlanta")
+boston = City.new("Boston")
+chicago = City.new("Chicago")
+denver = City.new("Denver")
+el_paso = City.new("El Paso")
+
+
+atlanta.add_route(boston, 100)
+atlanta.add_route(denver, 160)
+boston.add_route(denver, 180)
+boston.add_route(chicago, 120)
+chicago.add_route(el_paso, 80)
+denver.add_route(chicago, 40)
+denver.add_route(el_paso, 140)
+
+def dijkstra(starting_city, other_cities)
+    routes_from_city = {}
+    routes_from_city[starting_city] = [0, starting_city]
+
+    other_cities.each do |city|
+      routes_from_city[city] = [Float::INFINITY, nil]
+    end
+
+    visited_cities = []
+
+    current_city = starting_city
+
+    while current_city
+      visited_cities << current_city
+
+      current_city.routes.each do |city, price_info|
+        if routes_from_city[city][0] > price_info + routes_from_city[current_city][0]
+          routes_from_city[city] = [price_info + routes_from_city[current_city][0], current_city]
+        end
+      end
+
+
+      current_city = nil
+      cheapest_route_from_current_city = Float::INFINITY
+
+      routes_from_city.each do |city, rpice_info|
+        if price_info[0] < cheapest_route_from_current_city && !visited_cities.include?(city)
+          cheapest_route_from_current_city = price_info[0]
+          current_city = city
+        end
+      end
+    end
+
+    return routes_from_city
+end
