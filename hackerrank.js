c33618b43c4005714fe46313b2ce0369a3d05955
bartosian <bartosian1989@gmail.com>
2020-05-03 18:01:37 -0400

    Added new challenges

--- hackerrank.js
+++ hackerrank.js
@@ -19,9 +19,9 @@ function sockMerchant(n, ar) {
 // UDDDUDUUUDDDUDUU
 
 function countingValleys(n, s) {
-        let prevLevelCounter = 0,
-            currentLevelCounter = 0,
-            vallCounter = 0;
+    let prevLevelCounter = 0,
+        currentLevelCounter = 0,
+        vallCounter = 0;
 
     for(let i = 0; i < n; i++) {
         if(currentLevelCounter == -1 && prevLevelCounter == 0) {
@@ -31,8 +31,48 @@ function countingValleys(n, s) {
         prevLevelCounter = currentLevelCounter;
         currentLevelCounter += (s[i] == "D" ? -1 : 1);
     }
-    
+
     return vallCounter;
 }
 
-console.log(countingValleys(16, "UDDDUDUUUDDDUDUU"));
+function jumpingOnClouds(c) {
+    let cloudslength = c.length - 1,
+        jumpCount = 0,
+        currentCloud = 0;
+
+    while(cloudslength) {
+        let cloud = c[currentCloud + 2];
+        if(cloud !== undefined && cloud !== 1) {
+            currentCloud += 2;
+            cloudslength -= 2;
+        } else {
+            currentCloud += 1;
+            cloudslength -= 1;
+        }
+
+        jumpCount += 1;
+    }
+
+    return jumpCount;
+}
+
+function repeatedString(s, n) {
+    let repeatStringCount = Math.floor(n / s.length),
+        reminderOfString = n % s.length,
+        countPerString = 0,
+        result = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] == 'a') countPerString++;
+    }
+
+    result = repeatStringCount * countPerString;
+ 
+    for(let i = 0; i < reminderOfString; i++) {
+        if(s[i] == 'a') result++;
+    }
+
+    return result;
+}
+
+console.log(repeatedString("aba", 10));
