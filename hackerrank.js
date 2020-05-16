806b6946d747bf6833978ff278adaa376495e037
bartosian <bartosian1989@gmail.com>
2020-05-16 12:50:54 -0400

    Added solution to hackerrank

--- hackerrank.js
+++ hackerrank.js
@@ -730,3 +730,16 @@ class Student extends Person {
         return "O";
     }
 }
+
+class MyBook extends Book {
+    constructor(title="", author="", price="") {
+        super(title, author);
+        this.price = price;
+    }
+
+    display() {
+        console.log("Title: " + this.title);
+        console.log("Author: " + this.author);
+        console.log("Price: " + this.price);
+    }
+}
