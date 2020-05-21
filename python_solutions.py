4c08c97c9e8d1a383bfafb1e018c2227b1df1f48
bartosian <bartosian1989@gmail.com>
2020-05-21 11:44:33 -0400

    Added python tree solution

---
+++ python_solutions.py
@@ -0,0 +1,20 @@
+def insert(self, val):
+    if self.root == None:
+        self.root = Node(val)
+
+    root = self.root
+    while True:
+        if val > root.info:
+            if root.right:
+                root = root.right
+            else:
+                root.right = Node(val)
+                break
+        elif val < root.info:
+            if root.left:
+                root = root.left
+            else:
+                root.left = Node(val)
+                break
+        else:
+            break                 
