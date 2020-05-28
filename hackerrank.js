c71858688aeb38470038f20b58e79d996b657edd
bartosian <bartosian1989@gmail.com>
2020-05-28 07:32:05 -0400

    Added solution for linked lists

--- hackerrank.js
+++ hackerrank.js
@@ -1003,3 +1003,21 @@ let levelOrder = (root) => {
     }
 }; 
 
+let removeDuplicates=function(head){
+    if (typeof theactualHead === 'undefined') {
+        theactualHead = head;
+   }
+   if (head && head.next) {
+       if (head.data === head.next.data) {
+           if (head.next.next) {
+               head.next = head.next.next;
+           } else {
+               head.next = null;
+           }
+           this.removeDuplicates(head);
+       } else {
+           this.removeDuplicates(head.next);
+       }
+   }
+   return theactualHead;
+  }
