3e6912ab4f2365673d16802d14cc3bc3f676c87d
bartosian <bartosian1989@gmail.com>
2020-05-30 14:51:52 -0400

    Added solution for array manipulation

--- python_solutions.py
+++ python_solutions.py
@@ -30,3 +30,23 @@ def lca(root, v1, v2):
     if v1 < data and v2 < data:
         return lca(root.left, v1, v2)
     return root            
+
+
+def arrayManipulation(n, queries):
+    array = [0] * (n + 1)
+    
+    for query in queries: 
+        a = query[0] - 1
+        b = query[1]
+        k = query[2]
+        array[a] += k
+        array[b] -= k
+        
+    max_value = 0
+    running_count = 0
+    for i in array:
+        running_count += i
+        if running_count > max_value:
+            max_value = running_count
+            
+    return max_value
