3879ba95878423d1eb4bf829125e334f2400f5a5
bartosian <bartosian1989@gmail.com>
2020-07-29 20:30:02 -0400

    Added QUickUnion algorithm

--- unionFind.rb
+++ unionFind.rb
@@ -10,8 +10,6 @@ def main(num, pairs)
 end
 
 class QuickFindUF
-  attr_accessor :objects
-
   def initialize(n)
     @objects = (0..n).to_a
   end
@@ -27,5 +25,30 @@ class QuickFindUF
     @objects.each_with_index do |item, index|
       @objects[index] = qid if @objects[index] == pid
     end
+  end
+end
+
+class QuickUnionUF
+  def initialize(n)
+    @objects = (0..n).to_a
+  end
+
+  def connected?(p, q)
+    root(p) == root(q)
+  end
+
+  def union(p, q)
+    i = root(p);
+    j = root(q);
+    id[i] = j
+  end
+
+
+  private
+  def root(i)
+    while i != @objects[i]
+      i = @objects[i]
+    end
+     i
   end
 end
