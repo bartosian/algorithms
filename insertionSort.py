cde44ceef37cedc179669ee9a8d4a8037330976e
bartosian <bartosian1989@gmail.com>
2020-11-16 20:11:00 -0500

    Added insert sort in python

---
+++ insertionSort.py
@@ -0,0 +1,19 @@
+def insertion_sort():
+    for index in range(1, len(array)):
+
+        temp_value = array[index]
+        position = index - 1
+
+        while position >= 0:
+            if array[position] > temp_value:
+                array[position + 1] = array[postion]
+                postion = position - 1
+            else
+                break
+
+        array[position + 1] = temp_value
+
+    return array
+
+
+         
