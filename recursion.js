8b821792fc85975a064b978e301a00718731e8f3
bartosian <bartosian1989@gmail.com>
2020-12-02 09:09:17 -0500

    Added recursive even numbers solution

--- recursion.js
+++ recursion.js
@@ -3,3 +3,13 @@ function getTotalChars(list) {
 
   return list[0].length + getTotalChars(list.slice(1))
 }
+
+function getEvenNumbers(arr) {
+  if ( arr.length === 0 ) return []
+
+  if ( arr[0] % 2 === 0 ) {
+    return ([arr[0]]).concat(getEvenNumbers(arr.slice(1)))
+  } else {
+    return getEvenNumbers(arr.slice(1))
+  }
+}
