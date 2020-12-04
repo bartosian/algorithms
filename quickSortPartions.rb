5e430577712c6142954c036ce41ad7001b88a131
bartosian <bartosian1989@gmail.com>
2020-12-04 09:33:29 -0500

    Added quicjsort in ruby

--- quickSortPartions.rb
+++ quickSortPartions.rb
@@ -2,7 +2,7 @@ class SortableArray
   attr_reader :array
 
   def initialize(array)
-    @aray = array
+    @array = array
   end
 
   def partition!(left_pointer, right_pointer)
@@ -31,6 +31,22 @@ class SortableArray
 
     @array[left_pointer], @array[pivot_index] = @array[pivot_index], @array[left_pointer]
 
-    return left_pointer  
+    return left_pointer
+  end
+
+  def quicksort!(left_index, right_index)
+    if right_index - left_index <= 0
+      return
+    end
+
+    pivot_index = partition!(left_index, right_index)
+
+    quicksort!(left_index, pivot_index - 1)
+    quicksort!(pivot_index + 1, right_index)
   end
 end
+
+array = [0,5,2,1,6,3]
+sortable_array = SortableArray.new(array)
+sortable_array.quicksort!(0, array.length - 1)
+p sortable_array.array
