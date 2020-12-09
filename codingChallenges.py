1bc84e22026c5bcfe82167dce4e453975763a69a
bartosian <bartosian1989@gmail.com>
2020-12-08 22:16:46 -0500

    Added solution for sum in array

--- codingChallenges.py
+++ codingChallenges.py
@@ -5,3 +5,15 @@ def find_missing(input):
     actual_sum = (n * (n + 1)) / 2
 
     return actual_sum - sum_of_elements
+
+def find_sum_of_two(A, val):
+  hash = {};
+
+  for i in A:
+      j = val - i
+      if j in hash:
+          return True
+      else:
+          hash[i] = True
+
+  return False;
