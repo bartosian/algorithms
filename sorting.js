51af354e04e656cdcc11220127394cacf5505cb2
bartosian <bartosian1989@gmail.com>
2020-12-08 20:27:59 -0500

    Added merge sort solution

--- sorting.js
+++ sorting.js
@@ -146,3 +146,33 @@ function quickSelectInPlace(A, l, h, k) {
 function medianQuickselect(array) {
   return quickSelectInPlace(array, 0, array.length - 1, Math.floor(array.length/2))
 }
+
+
+function merge(leftA, rightA) {
+  var results = [],
+      leftIndex = 0,
+      rightIndex = 0;
+
+  while (leftIndex < leftA.length && rightIndex < rightA.length) {
+    if (leftA[leftIndex] < rightA[rightIndex]) {
+      results.push(leftA[leftIndex++]);
+    } else {
+      results.push(rightA[rightIndex++]);
+    }
+  }
+
+  var leftRemains = leftA.slice(leftIndex),
+      rightRemains = rightA.slice(rightIndex);
+
+  return results.concat(leftRemains).concat(rightRemains);
+}
+
+function mergeSort(array) {
+  if(array.length < 2) return array;
+
+  var midpoint = Math.floor(array.length / 2),
+      leftArray = array.slice(0, midpoint),
+      rightArray = array.slice(midpoint);
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
