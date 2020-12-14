974d80b59e5c43eb6960efdb7303c2a53f51443e
bartosian <bartosian1989@gmail.com>
2020-12-14 08:00:00 -0500

    Added quick union weighted solution for dynamic connectivity problem

--- quickUnion.js
+++ quickUnion.js
@@ -1,9 +1,11 @@
 class QuickUnionFind {
   constructor(n) {
     this._idsList = new Array(n);
+    this._size = new Array(n);
 
     for (let i = 0; i < this._idsList.length; i++) {
       this._idsList[i] = i;
+      this._size[i] = 1;
     }
   }
 
@@ -21,6 +23,16 @@ class QuickUnionFind {
   union(a, b) {
     let rootA = this._root(a),
         rootB = this._root(b);
+
+    if (rootA === rootB) return;
+
+    if (this._size[rootA] < this._size[rootB]) {
+      this._idsList[rootA] = rootB;
+      this._size[rootB] += this.size[rootA];
+    } else {
+      this._idsList[rootB] = rootA;
+      this._size[rootA] += this._size[rootB];
+    }
 
     this._idsList[rootA] = rootB;
   }
