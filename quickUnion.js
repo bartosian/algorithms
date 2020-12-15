b388863b7b7de1c404e05fc0cd9dbd79bc060ff6
bartosian <bartosian1989@gmail.com>
2020-12-14 20:03:10 -0500

    WQUPC with check for all connected

--- quickUnion.js
+++ quickUnion.js
@@ -11,7 +11,8 @@ class QuickUnionFind {
 
   _root(i) {
     while (this._idsList[i] !== i) {
-      i = this._idsList[i]
+      this._idsList[i] = this._idsList[this._idsList[i]];
+      i = this._idsList[i];
     }
     return i;
   }
@@ -35,5 +36,43 @@ class QuickUnionFind {
     }
 
     this._idsList[rootA] = rootB;
+  }
+}
+
+class WQUPC {
+  constructor(n) {
+    this._ids = new Array(n);
+    this._sizes = new Array(n).fill(1);
+    this._allConnected = false;
+
+    for (let i = 0; i < this._idsList.length; i++) {
+      this._idsList[i] = i;
+    }
+  }
+
+  _root(i) {
+    while (i !== this._idsList[i]) {
+      this._idsList[i] = this._idsList[this._idsList[i]];
+      i = this._idsList[i];
+    }
+  }
+
+  union(a, b) {
+    let rootA = this._root(a),
+        rootB = this._root(b),
+        sizeA = this._sizes[rootA],
+        sizeB = this._sizes[rootB];
+
+    if (sizeA < sizeB) {
+      this._idsList[rootA] = rootB;
+      this._sizes[rootB] += this._sizes[rootA];
+
+      if (this._sizes[rootB] === this._idsList.length) this._allConnected = true;
+    } else {
+      this._idsList[rootB] = rootA;
+      this._sizes[rootA] += this._sizes[rootB];
+
+      if (this._sizes[rootA] === this._idsList.length) this._allConnected = true;
+    }
   }
 }
