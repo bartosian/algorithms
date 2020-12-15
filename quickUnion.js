a458c8c8d8ee84ce451382b48b7df8fd444a13b9
bartosian <bartosian1989@gmail.com>
2020-12-14 20:51:21 -0500

    WQUPC with check for canonical value all connected

--- quickUnion.js
+++ quickUnion.js
@@ -2,10 +2,15 @@ class QuickUnionFind {
   constructor(n) {
     this._idsList = new Array(n);
     this._size = new Array(n);
+    this._grouped = new Array(n);
+    this._groupMax = new Array(n);
+    this.size = n;
 
     for (let i = 0; i < this._idsList.length; i++) {
       this._idsList[i] = i;
       this._size[i] = 1;
+      this._grouped[i] = [i];
+      this._groupMax[i] = i;
     }
   }
 
@@ -17,19 +22,27 @@ class QuickUnionFind {
     return i;
   }
 
+  find(i) {
+
+  }
+
   connected(a, b) {
     return this._root(a) === this._root(b);
   }
 
   union(a, b) {
     let rootA = this._root(a),
-        rootB = this._root(b);
+        rootB = this._root(b),
+        sizeA = this._sizes[rootA],
+        sizeB = this._sizes[rootB];
 
     if (rootA === rootB) return;
 
-    if (this._size[rootA] < this._size[rootB]) {
-      this._idsList[rootA] = rootB;
-      this._size[rootB] += this.size[rootA];
+    if (sizeA > sizeB) {
+      this._idsList[rootB] = rootA;
+      this._sizes[rootA] += sizeB;
+      this._groupMax[rootA] = Math.max(this._groupMax[rootA], this._groupMax[rootB]);
+      this._grouped[rootA] =
     } else {
       this._idsList[rootB] = rootA;
       this._size[rootA] += this._size[rootB];
@@ -44,9 +57,11 @@ class WQUPC {
     this._ids = new Array(n);
     this._sizes = new Array(n).fill(1);
     this._allConnected = false;
+    this._groupMax = new Array(n);
 
     for (let i = 0; i < this._idsList.length; i++) {
       this._idsList[i] = i;
+      this._groupMax[i] = i;
     }
   }
 
@@ -66,11 +81,13 @@ class WQUPC {
     if (sizeA < sizeB) {
       this._idsList[rootA] = rootB;
       this._sizes[rootB] += this._sizes[rootA];
+      this._groupMax[rootB] = Math.max(this._groupMax[rootA], this._groupMax[rootB]);
 
       if (this._sizes[rootB] === this._idsList.length) this._allConnected = true;
     } else {
       this._idsList[rootB] = rootA;
       this._sizes[rootA] += this._sizes[rootB];
+      this._groupMax[rootA] = Math.max(this._groupMax[rootA], this._groupMax[rootB]);
 
       if (this._sizes[rootA] === this._idsList.length) this._allConnected = true;
     }
