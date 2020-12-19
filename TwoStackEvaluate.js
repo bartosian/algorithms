8a1819bc76b89f75a4d3e2a83bc975cf27008549
bartosian <bartosian1989@gmail.com>
2020-12-18 20:31:31 -0500

    Two stack ald Dijkstra added

---
+++ TwoStackEvaluate.js
@@ -0,0 +1,61 @@
+const Stack = require('./arrayStack');
+
+const operands = {
+  '*': true,
+  '/': true,
+  '+': true,
+  '-': true
+};
+
+class Evaluate {
+
+  constructor() {
+    this.opStack = new Stack.ArrayStack();
+    this.valStack = new Stack.ArrayStack();
+  }
+
+  evaluate(expr) {
+    if (expr.length < 0) return false;
+
+    let exprList = expr.trim().split(" ");
+
+    for (let i = 0; i < exprList.length; i++) {
+      let exprChar = exprList[i].trim();
+      if (exprChar === '(' || exprChar === '') {
+        continue;
+      } else if (operands[exprChar]) {
+        this.opStack.push(exprChar);
+      } else if (exprChar === ')') {
+        let valA = this.valStack.pop(),
+            valB = this.valStack.pop(),
+            operator = this.opStack.pop();
+
+        switch (operator) {
+          case '+':
+            this.valStack.push(valB + valA);
+            break;
+          case '-':
+            this.valStack.push(valB - valA);
+            break;
+          case '*':
+            this.valStack.push(valB * valA);
+            break;
+          case '/':
+            this.valStack.push(valB / valA);
+            break;
+          default:
+            return false;
+        }
+
+      } else {
+        this.valStack.push(Number(exprChar));
+      }
+    }
+
+    return this.valStack.pop();
+  }
+}
+
+
+let evaluator = new Evaluate();
+const result = evaluator.evaluate("( 1 + ( ( ( 5 - 3 ) * 20 ) + ( 40 - ( 3 + 6 ) ) ) )");
--- arrayStack.js
+++ arrayStack.js
@@ -67,3 +67,5 @@ class ResizingArrayStack {
     }
   }
 }
+
+exports.ArrayStack = ArrayStack;
