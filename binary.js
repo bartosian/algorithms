7adfe207049362c86fb1d7da31e31e7e6d585291
bartosian <bartosian1989@gmail.com>
2020-12-19 20:18:21 -0500

    Bitonic array check value in logarithmic time

--- binary.js
+++ binary.js
@@ -17,4 +17,26 @@ function binarySearch(arr, key) {
   return -1;
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,8,9], 6));
+function binarySearchDesc(arr, key) {
+  let low = 0,
+      high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2),
+        midVal = arr[mid];
+
+    if (key === midVal) return mid;
+    if (key > midVal) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+}
+
+module.exports = {
+  binary: binarySearch,
+  descBinary: binarySearchDesc
+}
--- bitonicArray.js
+++ bitonicArray.js
@@ -1,12 +1,14 @@
+const search = require('./binary');
+
 function findMaximum(arr, low, high) {
-  if (low === high) return arr[low];
+  if (low === high) return low;
 
   if (high === low + 1 && arr[high] < arr[low]) {
-    return arr[low];
+    return low;
   }
 
   if (high === low + 1 && arr[high] > arr[low]) {
-    return arr[high];
+    return high;
   }
 
   let mid = Math.floor(low + (high - low) / 2);
@@ -19,4 +21,33 @@ function findMaximum(arr, low, high) {
     return findMaximum(arr, mid + 1, high);
   }
 
+}
+
+function searchBitonic(arr, n, key, index) {
+  if (key > arr[index]) {
+    return -1;
+  } else if (key === arr[index]) {
+    return index;
+  } else {
+    let ascBinSearch = search.binary(arr, key);
+
+    if (ascBinSearch !== -1) {
+      return ascBinSearch;
+    } else {
+      return search.descBinary(arr, key);
+    }
+  }
+}
+
+function main() {
+  let arr = [-8, 1, 2, 3, 4, 5, -2, -3],
+      key = 4,
+      n = arr.length,
+      l = 0,
+      r = n - 1;
+
+  let bitonicPoint = findMaximum(arr, 0, arr.length - 1);
+  let index = searchBitonic(arr, n, key, bitonicPoint);
+
+  return index;
 }
