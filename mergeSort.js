3ce33ef3dd0260034c9f0088ef658ff361973fa0
bartosian <bartosian1989@gmail.com>
2020-12-21 07:57:57 -0500

    Added merge sort algorithms

--- mergeSort.js
+++ mergeSort.js
@@ -1,25 +1,43 @@
-function merge(arr, aux, low, mid, high) {
-  for (let k = low; k < high; k++) {
-    aux[k] = arr[k];
+class Merge {
+  constructor(list) {
+    let arr = list,
+        len = list.length,
+        auxArr = new Array(len);
+
+    this._sort(arr, auxArr, 0, len - 1);
   }
 
-  let i = low,
-      j = mid + 1;
+  _merge(arr, aux,low, mid, high) {
+    for (let k = low; k <= high; k++) {
+      aux[k] = arr[k];
+    }
 
-  for (let k = low; k < high; k++) {
-    if (i > mid) {
-      arr[k] = aux[j++];
-    } else if (j > high) {
-      arr[k] = aux[i++];
-    } else if (aux[i] < aux[j]) {
-      arr[k] = aux[i++];
-    } else {
-      arr[k] = aux[j++];
+    let i = low,
+        j = mid + 1;
+
+    for (let k = low; k <= high; k++) {
+      if (i > mid) {
+        arr[k] = aux[j++];
+      } else if (j > high) {
+        arr[k] = aux[i++];
+      } else if (aux[i] > aux[j]) {
+        arr[k] = aux[j++];
+      } else {
+        arr[k] = aux[i++];
+      }
     }
   }
+
+  _sort(arr, aux, low, high) {
+    if (high <= low) {
+      return;
+    }
+
+    let mid = Math.floor(low + (high - low) / 2);
+
+    this._sort(arr, aux, low, mid);
+    this._sort(arr, aux, mid + 1, high);
+
+    this._merge(arr, aux, low, mid, high);
+  }
 }
-
-let arr = [1,3,5,7,9,11,14,20,31,50];
-merge(arr, [], 0, 5, 9);
-
-console.log(arr);
