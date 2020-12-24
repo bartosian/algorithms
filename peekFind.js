39f268c6841f05392a78009bf5e8413ede424002
bartosian <bartosian1989@gmail.com>
2020-12-24 11:13:31 -0500

    Added peak search algorithm in 2dim array

--- peekFind.js
+++ peekFind.js
@@ -13,4 +13,39 @@ class Peek {
       return Peek.oneDimension(arr, mid + 1, mid - 1, high, n);
     }
   }
+
+  static twoDimension(arr, rows, columns, mid) {
+    let initMax = 0;
+    let [max, max_index] = Peek.findMaximum(arr, rows, mid, initMax);
+
+    if (mid === 0 || mid === columns - 1) {
+      return max;
+    }
+
+    if (max >= arr[max_index][mid - 1] && max >= arr[max_index][mid + 1]) {
+      return max;
+    }
+
+    if (max < arr[max_index][mid - 1]) {
+      return Peek.twoDimension(arr, rows, columns, mid - Math.ceil(mid / 2));
+    }
+
+    return Peek.twoDimension(arr, rows, columns, mid + Math.ceil(mid / 2));
+  }
+
+  static findMaximum(arr, rows, mid, max) {
+    let max_index = 0;
+    for (let i = 0; i < rows; i++) {
+      if (arr[i][mid] > max) {
+        max_index = i;
+        max = arr[i][mid];
+      }
+    }
+
+    return [max, max_index];
+  }
+
+  static findPeek(arr, columns, rows) {
+    return Peek.twoDimension(arr, rows, columns, Math.floor(columns / 2));
+  }
 }
