9a69616a55135fc199e1258f9e1b8c81d7c465d0
bartosian <bartosian1989@gmail.com>
2020-12-26 17:10:31 -0500

    Added shuffle linked list alg

--- shuffleLinkedList.js
+++ shuffleLinkedList.js
@@ -1,27 +1,7 @@
 class LinkedList {
-  static shuffle(head, n) {
-    if (n === 1) return;
-
-    let k = 1,
-        mid = head,
-        midN = Math.floor(n / 2);
-
-    while (k < midN) {
-      mid = mid.next;
-      k++;
-    }
-
-    rh = mid.next;
-    mid.next = null;
-
-    LinkedList.shuffle(head, midN);
-    LinkedList.shuffle(rh, N - midN);
-    merge(head, rh);
-  }
-
   static merge(lh, rh) {
-    let leftNode = lh,
-        rightNode = rh;
+    let left = lh,
+        right = rh;
 
     if (Math.round(Math.random()) === 1) {
       lh = right;
@@ -30,6 +10,45 @@ class LinkedList {
       left = left.next;
     }
 
-    let runner = lh;    
+    let pointer = lh;
+
+    while (left.next != null || right.next != null) {
+      if (left.next === null) {
+          pointer.next = right;
+          right = right.next;
+      } else if (right.next === null) {
+        pointer.next = left;
+        left = left.next;
+      } else if (Math.round(Math.random()) === 1) {
+        pointer.next = right;
+        right = right.next;
+      } else {
+        pointer.next = left;
+        left = left.next;
+      }
+
+      pointer = pointer.next;
+    }
+  }
+
+  static shuffle(head, n) {
+    if (n === 1) return;
+
+    let mid = Math.floor(n / 2),
+        midNode = head;
+
+    for (let i = 0; i < mid; i++) {
+      midNode = midNode.next;
+    }
+
+    let lh = head,
+        rh = midNode.next;
+
+    midNode.next = null;
+
+    LinkedList.shuffle(lh, mid);
+    LinkedList.shuffle(rh, n - mid);
+
+    LinkedList.merge(lh, rh);
   }
 }
