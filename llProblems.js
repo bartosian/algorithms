93f2ee048ee2ac764581a1f584d975ecd139c4f3
bartosian <bartosian1989@gmail.com>
2021-01-08 21:21:24 -0500

    Added not stable solution for partitioning linked list

--- llProblems.js
+++ llProblems.js
@@ -64,4 +64,64 @@ class LinkedList {
     node.item = node.next.item;
     node.next = node.next.next;
   }
+
+  partition(head, k) {
+    if (!head.next) return false;
+
+    let beforeHead = beforeTail = afterHead = afterTail = null;
+
+    while (head) {
+      let next = head.next;
+      head.next = null;
+
+      if (head.item < k) {
+        if (!beforeHead) {
+          beforeHead = beforeTail = head;
+        } else {
+          beforeTail.next = head;
+          beforeTail = head;
+        }
+      } else {
+        if (!afterHead) {
+          afterHead = afterTail = head;
+        } else {
+          afterTail.next = head;
+          afterTail = head;
+        }
+      }
+
+      head = next;
+    }
+
+    if (!beforeHead) {
+      return afterHead;
+    }
+
+    beforeTail.next = afterHead;
+    return afterHead;
+  }
+
+  partition2(node, k) {
+    let head = tail = node;
+
+    node = node.next;
+
+    while (node) {
+      let next = node.next;
+
+      if (node.item < k) {
+        node.next = head;
+        head = node;
+      } else {
+        tail.next = node;
+        tail = node;
+      }
+
+      node = next;
+    }
+
+    tails.next = null;
+
+    return head;
+  }
 }
