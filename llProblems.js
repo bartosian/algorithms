9c549232cd15b6ddf154b60aed216e82bbc5e708
bartosian <bartosian1989@gmail.com>
2021-01-16 18:26:55 -0500

    Added loop detection in linked list solution

--- llProblems.js
+++ llProblems.js
@@ -345,4 +345,28 @@ class LinkedListIntersection {
 
     return current;
   }
+
+  findBeginningOfLoop(head) {
+    let slow = fast = head;
+
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (Object.is(fast, slow)) {
+        break;
+      }
+    }
+
+    if (!fast || !fast.next) return null;
+
+    slow = head;
+
+    while(!Object.is(slow, fast)) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    return fast;
+  }
 }
