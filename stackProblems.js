bad6d3b1a6826f9a1edcb41935771d3b7061914d
bartosian <bartosian1989@gmail.com>
2021-01-17 16:33:41 -0500

    Added solution for setOfStacks problem

--- stackProblems.js
+++ stackProblems.js
@@ -100,3 +100,100 @@ class StackWithMin {
     return this.min[this.size - 1];
   }
 }
+
+class SetOfStacks {
+  constructor(size) {
+    this.capacity = size;
+    this.stacks = new Stack();
+  }
+
+  getLastStack() {
+    if (this.stacks.size()) return null;
+
+    return this.stacks.peek();
+  }
+
+  push(value) {
+    let lastStack = this.getLastStack();
+
+    if (lastStack && !lastStack.isFull) {
+      lastStack.push(value);
+    } else {
+      let newStack = new Stack();
+      newStack.push(value);
+
+      this.stacks.push(newStack);
+    }
+  }
+
+  pop() {
+    let lastStack = this.getLastStack();
+
+    if (!lastStack) {
+      throw new EmptyStackExecption();
+    }
+
+    let value = lastStack.pop();
+
+    if (lastStack.isEmpty) {
+      this.stacks.pop();
+    }
+
+    return value;
+  }
+
+  isEmpty() {
+   let lastStack = this.getLastStack();
+
+   if (!this.stack.size() || !lastStack) return true;
+
+   return false;
+  }
+
+  popAt(stackId) {
+    return this.shiftLeft(stackId, true);
+  }
+
+  shiftLeft(stackId, removeTop) {
+    let stack = this.stacks[stackId],
+        removedItem = 0;
+
+    if (removeTop) {
+      removedItem = stack.pop();
+    } else {
+      removedItem = stack.removeBottom();
+    }
+
+    if (stack.isEmpty) {
+      this.stacks.pop();
+    } else if (this.stacks.size() > stackId + 1) {
+      let value = this.shiftLeft(stackId + 1, false);
+      stack.push(v);
+    }
+
+    return removedItem;
+  }
+}
+
+clas Stack {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.size = 0;
+    this.values = [];
+  }
+
+  isFull() {
+    return this.size === this.capacity;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  push(value) {
+    if (this.size === this.capacity) return false;
+
+    this.values.push(value);
+    this.size++;
+  }
+}
