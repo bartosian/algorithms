1810daa9e8c5bad4a6262b7b8c43b9b1005d627a
bartosian <bartosian1989@gmail.com>
2021-01-26 21:27:56 -0500

    Added BST symbol table implementation

---
+++ BST.js
@@ -0,0 +1,139 @@
+class Node {
+  constructor(key, value, size) {
+    this.key = key;
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.size = 1;
+  }
+}
+
+class BST {
+  constructor(node) {
+      this.root = node;
+  }
+
+  put(node=this.root, key, value) {
+    if (!node) {
+      return new Node(key, value, 1);
+    }
+
+    if (key < node.key) {
+      node.left = this.put(node.left, key, value);
+    } else if (key > node.key) {
+      node.right = this.put(node.right, key, value);
+    } else {
+      node.value = value;
+    }
+
+    node.count = 1 + this.size(node.left) + this.size(node.right);
+
+    return node;
+  }
+
+  size() {
+    return this._size(this.root);
+  }
+
+  _size(node) {
+    if (!node) return 0;
+
+    return node.size;
+  }
+
+  min() {
+    let node = this.root;
+
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.value;
+  }
+
+  max() {
+    let node = this.root;
+
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.value;
+  }
+
+  get(key) {
+    let node = this.root;
+
+    while(node) {
+      if (key < node.key) {
+        node = node.left;
+      } else if (key > node.key) {
+        node = node.right;
+      } else {
+        return node.value;
+      }
+    }
+
+    return null;
+  }
+
+  floor(key) {
+    let node = this._floor(this.root, key);
+
+    if (node) {
+      return node value;
+    }
+
+    return null;
+  }
+
+  _floor(node, key) {
+    if (!node) return null;
+
+    if (key === node.key) return node;
+
+    if (key < node.key) {
+      return this._floor(node.left, key);
+    } else (key > node.key) {
+      let t = this._floor(node.right, key);
+
+      if (!t) {
+        return node;
+      } else {
+        return t;
+      }
+    }
+  }
+
+  rank(key) {
+    return this._rank(key, this.root);
+  }
+
+  _rank(key, node) {
+    if (key < node.key) {
+      return this._rank(key, node.left);
+    } else if (key > node.right) {
+      return 1 + this.size(node.left) + this._rank(key, node.right)
+    } else {
+      return this.size(node.left);
+    }
+  }
+
+  keys() {
+    let queue = new Queue();
+
+    this.inorder(this.root, queue);
+    return queue;
+  }
+  
+  // return in ascending order
+  inorder(node, queue) {
+    this.inorder(node.left, queue);
+    queue.enqueue(node.key);
+    this.inorder(node.right, queue);
+  }
+
+  delete(key) {
+
+  }
+}
