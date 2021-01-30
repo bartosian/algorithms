17eb29a70a64ba019ceafe591a2e49cfdf32f095
bartosian <bartosian1989@gmail.com>
2021-01-29 19:33:48 -0500

    Added common ancestor problem solution

--- firstCommonAncestor.js
+++ firstCommonAncestor.js
@@ -1,6 +1,7 @@
 // O(d)
 class CommonAncestor {
-  constructor(nodeA, nodeB) {
+  constructor(root, nodeA, nodeB) {
+    this.root = root;
     this.nodeA = nodeA;
     this.nodeB = nodeB;
   }
@@ -42,5 +43,62 @@ class CommonAncestor {
     }
 
     return node;
+  }
+
+  findCommonAnc() {
+    let root = this.root,
+        nodeA = this.nodeA,
+        nodeB = this.nodeB;
+
+    if (!this.covers(root, nodeA) || !this.covers(root, nodeB)) {
+      return null;
+    }
+
+    return this.ancestorHelper(root, nodeA, nodeB);
+  }
+
+  ancestorHelper(root, nodeA, nodeB) {
+    if (!root || root === nodeA || root === nodeB) {
+      return root;
+    }
+
+    let isOnLeftA = this.covers(root.left, nodeA),
+        isOnLeftB = this.covers(root.left, nodeB);
+
+    if (isOnLeftA !== isOnLeftB) {
+      return root;
+    }
+
+    childSie = isOnLeftA ? root.left : root.right;
+
+    return this.ancestorHelper(childSie, nodeA, nodeB);
+  }
+
+  covers(root, node) {
+    if (root === node) return true;
+    if (!root) return false;
+
+    return this.covers(root.left, node) || this.covers(root.right, node);
+  }
+
+  commonAncestor(root, p, q) {
+    if (!root) return null;
+    if (root === p && root === q) return root;
+
+    let ancA = this.commonAncestor(root.left, p, q);
+
+    if (ancA && ancA !== p && ancA !== q) return ancA;
+
+    let ancB = this.commonAncestor(root.right, p, q);
+
+    if (ancB && ancB !== p && ancB !== q) return ancB;
+
+    if (ancA && ancB) {
+      return root;
+    } else if (ancA === p || ancB === q) {
+      return root;
+    } else {
+      return !ancA ? ancB : ancA;
+    }
   }
 }
