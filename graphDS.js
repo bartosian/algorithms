2ee088fd34e8af31c289b54b148b283078e23366
bartosian <bartosian1989@gmail.com>
2021-02-02 09:27:06 -0500

    Added DFS data structure

--- graphDS.js
+++ graphDS.js
@@ -46,3 +46,47 @@ class Graph {
     return this.edges * 2 / this.verteces;
   }
 }
+
+class DepthFirstPaths {
+  constructor(graph, s) {
+    this.graph = graph;
+    this.s = s;
+    this.marked = [];
+    this.edgeTo = [];
+
+    this.dfs(this.s);
+  }
+
+  // after DFS in O(1)
+  hasPathTo(vertex) {
+    return this.marked[vertex];
+  }
+
+  pathTo(vertex) {
+    if (!this.marked[vertex]) {
+      return null;
+    }
+
+    let path = [];
+
+    for (let i = vertex; i != this.s; i = this.edgeTo(i)) {
+      path.push(i);
+    }
+
+    path.push(this.s);
+
+    return path;
+  }
+
+  dfs(vertex) {
+    let adj = this.graph.adj(vertex);
+    this.marked[vertex] = true;
+
+    for (let i of adj) {
+      if (!this.marked[i]) {
+        this.dfs(i);
+        this.edgeTo[i] = vertex;
+      }
+    }
+  }
+}
