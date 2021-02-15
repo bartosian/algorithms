df7b3ddf9c7bd441f05d43d9a9ed08bf3ff42ed0
bartosian <bartosian1989@gmail.com>
2021-02-14 19:14:15 -0500

    Added solution for getting path in grid

--- recursive.js
+++ recursive.js
@@ -35,3 +35,36 @@ class WaysCounter {
     }
   }
 }
+
+class Path {
+  constructor(maze) {
+    this.maze = maze;
+
+    if (!this.maze || !this.maze.length) return null;
+
+    let path = [];
+
+    if (this.getPath(this.maze.length - 1, this.maze[0].length - 1, path)) {
+      return path;
+    }
+
+    return null;
+  }
+
+  getPath(row, col, path) {
+    if (row < 0 || col < 0 || !this.maze[row][col]) {
+      return false;
+    }
+
+    let isOrigin = (row === 0) && (col === 0);
+
+    if (isOrigin || this.getPath(row, col - 1, path) || this.getPath(row - 1, col, path)) {
+      let point = new Point(row, col);
+      path.push(point);
+
+      return true;
+    }
+
+    return false;
+  }
+}
