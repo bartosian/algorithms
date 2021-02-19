6363e332cc9c60c341b75345ca44a2c2bcb0b0d6
bartosian <bartosian1989@gmail.com>
2021-02-18 20:30:04 -0500

    Added key indexed counting algorithm

--- stringAlgs.js
+++ stringAlgs.js
@@ -18,3 +18,45 @@ function suffixes(str) {
 
   return suffixes;
 }
+
+function lcp(strA, strB) {
+  let len = Math.min(strA.length, strB.length);
+
+  for (let i = 0; i < len; i++) {
+    if (strA.charAt(i) !== strB.charAt(i)) {
+      return i;
+    }
+  }
+
+  return len;
+}
+
+function keyIndexedCount(arr, radix) {
+  let len = arr.length,
+      count = new Array(radix + 1).fill(0),
+      aux = [];
+
+  for (let i = 0; i < len; i++) {
+    count[arr[i] + 1] += 1;
+  }
+
+  console.log(count);
+
+  for (let r = 0; r < radix; r++) {
+    count[r + 1] += count[r];
+  }
+
+  console.log(count);
+
+  for (let j = 0; j < len; j++) {
+    aux[count[arr[j]]++] = arr[j];
+  }
+
+  console.log(aux);
+
+  for (let i = 0; i < len; i++) {
+    arr[i] = aux[i];
+  }
+
+  console.log(arr);
+}
