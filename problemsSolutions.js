419ee5cae5e01a264231087e0b7c54a846973641
bartosian <bartosian1989@gmail.com>
2021-02-27 21:11:57 -0500

    Added DP solution

--- problemsSolutions.js
+++ problemsSolutions.js
@@ -297,3 +297,21 @@ class NumberOfIslandsBFS {
     return this.count;
   }
 }
+
+var maximalSquare = function(matrix) {
+    let rows = matrix.length,
+        columns = matrix[0] ? matrix[0].length : 0,
+        maxSide = 0,
+        aux = new Array(rows + 1).fill(0).map(row => new Array(columns + 1).fill(0));
+
+    for (let i = 1; i <= rows; i++) {
+        for (let j = 1; j <= columns; j++) {
+            if (matrix[i - 1][j - 1] === "1") {
+                aux[i][j] = Math.min(Math.min(aux[i][j - 1], aux[i - 1][j]), aux[i - 1][j - 1]) + 1;
+                maxSide = Math.max(maxSide, aux[i][j]);
+        }
+    }
+  }
+
+    return maxSide * maxSide;
+};
