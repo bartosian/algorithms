89010671e59e8d1d6b37d2609eef53e7e6d8b680
bartosian <bartosian1989@gmail.com>
2021-03-12 13:20:43 -0500

    Added solution for robot in circle problem

--- criticalConnection.js
+++ criticalConnection.js
@@ -59,3 +59,25 @@ class CriticalConnections {
     }
   }
 }
+
+
+function isRobotBounded(instructions) {
+  // north = 0, east = 1, south = 2, west = 3
+  const directions = [[0, 1],[1, 0],[0,-1],[-1,0]];
+  let x = 0,
+      y = 0,
+      idx = 0;
+
+  for (let i = 0; i < instructions.length; i++) {
+    if (instructions[i] === "L") {
+      idx = (idx + 3) % 4;
+    } else if (instructions[i] === "R") {
+      idx = (idx + 1) % 4;
+    } else {
+      x += directions[idx][0];
+      y += directions[idx][1];
+    }
+  }
+
+  return (x === 0 && y === 0) || idx !== 0;    
+}
