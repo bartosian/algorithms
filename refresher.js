d504a9e4e671683e29329ccd2a122485d8161aac
bartosian <bartosian1989@gmail.com>
2021-03-14 18:01:56 -0400

    Add stair case solution

--- refresher.js
+++ refresher.js
@@ -43,5 +43,44 @@ var jump = function(nums) {
     }
   }
 
-  return jumps[jumps.length - 1];
+  return jumps[jumps.length - 1] !== Number.MAX_SAFE_INTEGER;
+};
+
+var climbStairsA = function(i, n, memo={}) {
+  if (i > n) return 0;
+  if (i === n) return 1;
+
+  if (memo[i] > 0) {
+    return memo[i];
+  }
+
+  memo[i] = climbStairs(i + 1, n, memo) + climbStairs(i + 2, n, memo);
+  return memo[i];
+};
+
+var climbStairsE = function(n) {
+  let dp = new Array(n + 1);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let j = 3; j <= n; j++) {
+    dp[j] = dp[j - 1] + dp[j - 2];
+  }
+
+  return dp[n];
+};
+
+var climbStairs = function(n) {
+  let first = 1,
+      second = 2;
+
+  if (n === 1) return first;
+
+  for (let j = 3; j <= n; j++) {
+    let prev = second;
+    second = first + second;
+    first = prev;
+  }
+
+  return second;
 };
