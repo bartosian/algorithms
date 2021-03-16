75013bf86d685f9053816a5c2923dba4e53a3a58
bartosian <bartosian1989@gmail.com>
2021-03-16 16:24:51 -0400

    Added solution for list with random pointer and constant space

--- listWithPointer.js
+++ listWithPointer.js
@@ -29,3 +29,38 @@ class RandomListCopy {
     return newNode;
   }
 }
+
+class RandomListCopyWithoutMap {
+  copy(head) {
+    if (!head) return null;
+
+    let newList = head;
+
+    while (newList) {
+      let newNode = new Node(newList.val);
+      newNode.next = newList.next;
+      newList.next = newNode;
+      newList = newNode.next;
+    }
+
+    newList = head;
+
+    while (newList) {
+      newList.next.random = newList.random ? newList.random.next : null;
+      newList = newList.next.next;
+    }
+
+    let oldList = head,
+        newlist = head.next,
+        headOld = head.next;
+
+    while (oldList) {
+      oldList.next = oldList.next.next;
+      newlist.next = newlist.next ? newlist.next.next : null;
+      oldList = oldList.next;
+      newlist = newlist.next;
+    }
+
+    return headOld;
+  }
+}
