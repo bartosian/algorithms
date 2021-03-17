818d47454dc73893e24a553392050342efe1def0
bartosian <bartosian1989@gmail.com>
2021-03-17 15:24:18 -0400

    Added word search solution

--- trie.js
+++ trie.js
@@ -32,7 +32,7 @@ class TrieNode {
   }
 }
 
-class Trie {
+class Trie2 {
   constructor() {
     this.root = new TrieNode();
   }
@@ -93,7 +93,7 @@ class Trie {
       if (node[c] == null) node[c] = {};
       node = node[c];
     }
-    node.isWord = true;
+    node.word = word;
   }
 
   traverse(word) {
@@ -107,10 +107,79 @@ class Trie {
 
   search(word) {
     const node = this.traverse(word);
-    return node != null && node.isWord === true;
+    return node != null && node.word !== undefined;
   }
 
   startsWith(prefix) {
     return this.traverse(prefix) != null;
   }
 }
+
+class Solution {
+    constructor() {
+        this.result = [];
+        this.trie = new Trie();
+        this.board = null;
+    }
+
+    findWords(board, words) {
+        let root = this.trie.root;
+
+        for (let word of words) {
+            this.trie.insert(word);
+        }
+
+        this.board = board;
+        for (let row = 0; row < board.length; row++) {
+            for (let col = 0; col < board[row].length; col++) {
+                if (root[board[row][col]]) {
+                    this.backtracking(row, col,root);
+                }
+            }
+        }
+
+        return this.result;
+    }
+
+    backtracking(row, col, parent) {
+        let letter = this.board[row][col],
+            currNode = parent[letter];
+
+        console.log(letter);
+        console.log(currNode);
+        console.log(currNode.word);
+        if (currNode.word) {
+            this.result.push(currNode.word);
+            currNode.word = null;
+        }
+
+        this.board[row][col] = "#";
+
+        let rowOffset = [-1, 0, 1, 0],
+            colOffset = [0, 1, 0, -1];
+
+        for (let i = 0; i < 4; i++) {
+            let newRow = row + rowOffset[i],
+                newCol = col + colOffset[i];
+
+            if (newRow < 0 || newRow >= this.board.length || newCol < 0 || newCol >= this.board[0].length) continue;
+
+            if (currNode[this.board[row][col]]) this.backtracking(newRow, newCol, currNode);
+        }
+
+        this.board[row][col] = letter;
+
+        if (Object.keys(currNode).length === 0) {
+            delete parent[letter];
+        }
+    }
+}
+
+
+var findWords = function(board, words) {
+    let solution = new Solution();
+
+    return solution.findWords(board, words);
+};
+
+console.log(findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["oath","pea","eat","rain"]));
