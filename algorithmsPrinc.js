70abb974bf449bdc10483afa2decaa4d791f4e01
bartosian <bartosian1989@gmail.com>
2021-03-24 10:34:53 -0400

    Added binary search solution

--- algorithmsPrinc.js
+++ algorithmsPrinc.js
@@ -6,3 +6,17 @@ function greatestCommonDivisor(p, q) {
 
   return greatestCommonDivisor(q, r);
 }
+
+function binarySearch(key, array, lo, hi) {
+  if (lo > hi) return -1;
+
+  let mid = Math.floor(lo + (hi -lo) / 2);
+
+  if (key < array[mid]) {
+    return binarySearch(key, array, lo, mid - 1);
+  } else if (key > array[mid]) {
+    return binarySearch(key, array, mid + 1, hi);
+  } else {
+    return array[mid];
+  }
+}
