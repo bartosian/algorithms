6830e925cc078232b239b466693dca09ba8f81dd
bartosian <bartosian1989@gmail.com>
2021-04-05 13:57:11 -0400

    Added solution for water trap problem using DP

--- algoSolutions.js
+++ algoSolutions.js
@@ -13,4 +13,31 @@ class Solution {
 
     return len > 0 && lists[0] || false;
   }
+
+  trap(heights) {
+    if (!heights.length) return 0;
+
+    let size = heights.length,
+        leftMax = new Array(size),
+        rightMax = new Array(size),
+        ans = 0;
+
+    leftMax[0] = heaights[0];
+
+    for (let i = 1; i < size; i++) {
+      leftMax[i] = Math.max(heights[i], leftMax[i - 1]);
+    }
+
+    rightMax[size - 1] = heights[size - 1];
+
+    for (let i = size - 2; i >= 0; i--) {
+      rightMax[i] = Math.max(heights[i], rightMax[i + 1]);
+    }
+
+    for (let j = 0; j < size; j++) {
+      ans += Math.min(leftMax[j], rightMax[j]) - heights[j];
+    }
+
+    return ans;
+  }
 }
