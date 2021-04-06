98cb94700c8748a8f1a3ddbe9a93b37d8bfb6a1a
bartosian <bartosian1989@gmail.com>
2021-04-06 12:31:17 -0400

    Added overlapping intervals check

--- algoSolutions.js
+++ algoSolutions.js
@@ -199,5 +199,17 @@ function mergeIntervals(intervals) {
     }
   }
 
-  return results;    
+  return results;
 }
+
+var canAttendMeetings = function(intervals) {
+    if (!intervals.length) return true;
+
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    for (let i = 0; i < intervals.length - 1; i++) {
+        if (intervals[i][1] > intervals[i + 1][0]) return false;
+    }
+
+    return true;
+};
