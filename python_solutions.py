cf0254f664dc23ee71f39ebeec98d58ff34be79f
bartosian <bartosian1989@gmail.com>
2021-11-27 22:01:27 -0500

    Added solutions 4

--- python_solutions.py
+++ python_solutions.py
@@ -47,3 +47,68 @@ class Solution:
             seen[val] = i
 
         return output
+
+
+class ListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.dict = dict()
+        self.size = 0
+
+    def get(self, key: int) -> int:
+        node = self.dict.get(key)
+
+        if node:
+            self.removeFromList(node)
+            self.insertIntoHead(node)
+
+            return node.value
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        node = self.dict.get(key)
+
+        if node:
+            self.removeFromList(node)
+            self.insertIntoHead(node)
+            node.value = value
+        else:
+            if self.size >= self.capacity:
+                self.removeFromTail()
+
+            node = ListNode(key, value)
+            self.insertIntoHead(node)
+            self.dict[key] = node
+            self.size += 1
+
+    def removeFromList(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def insertIntoHead(self, node):
+        headNext = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = headNext
+        headNext.prev = node
+
+    def removeFromTail(self):
+        if self.size == 0:
+            return
+        tail_node = self.tail.prev
+        del self.dict[tail_node.key]
+        self.removeFromList(tail_node)
+        self.size -= 1
