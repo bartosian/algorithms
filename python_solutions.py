5712d7a4289de40367a3a0573f687fe8e463b11c
bartosian <bartosian1989@gmail.com>
2021-11-27 22:26:29 -0500

    Added solutions 5

--- python_solutions.py
+++ python_solutions.py
@@ -48,6 +48,19 @@ class Solution:
 
         return output
 
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
+
+        intervals.sort(key=lambda x: x[0])
+
+        for i in intervals:
+            if not merged or merged[-1][-1] < i[0]:
+                merged.append(i)
+            else:
+                merged[-1][-1] = max(merged[-1][-1], i[-1])
+
+        return merged
+
 
 class ListNode:
     def __init__(self, key, value):
