0873a9e69aae63b9840dd7be82140b2de77df3dd
bartosian <bartosian1989@gmail.com>
2021-11-28 20:15:25 -0500

    Added solutions for dp

--- python_solutions.py
+++ python_solutions.py
@@ -125,3 +125,33 @@ class LRUCache:
         del self.dict[tail_node.key]
         self.removeFromList(tail_node)
         self.size -= 1
+
+    def rob_memo(self, nums: List[int]) -> int:
+        def dp(i):
+            """Calculate result for given step"""
+
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
+            if i not in memo:
+                memo[i] = max(dp(i - 1), dp(i - 2) + nums[i])
+
+            return memo[i]
+
+        memo = {}
+        return dp(len(nums) - 1)
+
+    def rob_tab(self, nums: List[int]) -> int:
+        if len(nums) == 1;
+            return nums[0]
+
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        return dp[-1]    
