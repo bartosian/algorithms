56446fe30dfb3dc2b3273230de5406774e2f9754
bartosian <bartosian1989@gmail.com>
2021-11-30 11:11:46 -0500

    Added insertion sort solution

--- python_solutions.py
+++ python_solutions.py
@@ -291,3 +291,18 @@ class LRUCache:
                 break
 
         return arr
+
+    def insertion_sort(arr):
+        n = len(arr)
+
+        for i in range(1, n):
+            item = arr[i]
+            j = i - 1
+
+            while j >= 0 and arr[j] > item:
+                arr[j + 1] = arr[j]
+                j -= 1
+
+            arr[j + 1] = item
+
+        return arr
