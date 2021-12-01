6280f5ce9929b75b4ac98b50c00ec982bbf51276
bartosian <bartosian1989@gmail.com>
2021-12-01 14:51:37 -0500

    Added maxProfit solution

--- python_solutions.py
+++ python_solutions.py
@@ -409,3 +409,13 @@ class LRUCache:
                 return False
             
         return len(s_result) == 0        
+
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit, min_price = 0, float('inf')
+        
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+            
+        return max_profit        
