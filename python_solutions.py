b4587d00e3a36157059fc6ff7103ec04987a79ef
bartosian <bartosian1989@gmail.com>
2021-12-02 15:05:44 -0500

    Added max subarra solution

--- python_solutions.py
+++ python_solutions.py
@@ -436,4 +436,14 @@ class LRUCache:
             else:
                 count += 1
                 
-        return count        
+        return count
+
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+
+        for i in range(1, n):
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+
+        return max(dp)
