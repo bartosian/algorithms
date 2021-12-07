557fab3b2f40222665482ef9130194a0e3d1753f
bartosian <bartosian1989@gmail.com>
2021-12-06 21:18:30 -0500

    Added solution for merging arrays without additional space

--- python_solutions.py
+++ python_solutions.py
@@ -629,3 +629,22 @@ class LRUCache:
             
             todo.pop(n)
         return not todo
+
+    def mergeArray(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+        
+        for i in range(p, -1, -1):
+            if p2 < 0: break
+                
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[i] = nums2[p2]
+                p2 -= 1        
